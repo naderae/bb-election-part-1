6b8e21c271fbe90f7a30328bc37d44626f9ca435
naderae <naderabouezze93@gmail.com>
2017-08-22 16:00:39 -0400

    done

--- election.js
+++ election.js
@@ -1,5 +1,16 @@
 document.addEventListener("DOMContentLoaded", function() {
 
-  // Imagination!
 
+
+      $.ajax({
+      url: 'https://bb-election-api.herokuapp.com/',
+      method: 'GET',
+      dataType: 'json'
+    }).done(function(responseData){
+        responseData.candidates.forEach(function(candidate){
+          var individualCandidate = document.createElement('li')
+          individualCandidate.innerHTML = candidate.name + " with " + candidate.votes + " votes"
+          document.getElementById('list-of-candidates').append(individualCandidate)
+        })
+      });
 });
